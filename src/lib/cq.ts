210b8da65577a5c7d4188d3f39415c31b8fc5f23
zlq4863947 <zlq4863947@gmail.com>
2017-12-08 00:20:55 +0900

    代码修正

--- src/lib/cq.ts
+++ src/lib/cq.ts
@@ -9,7 +9,7 @@ export const candlestick_5min = (db: string) => `
         min(price) AS low,
         first(price) AS open,
         last(price) AS close,
-        sum(amount) AS volume
+        sum(volume) AS volume
       INTO ${Enums.Measurement.Candlestick_5min}
       FROM ${Enums.Measurement.Tick}
       GROUP BY time(5m), symbol
